0a248998e97731469c438841e5d0319e9aaf681f
NIK2703 <nikita.ogurnoy@mail.ru>
2026-06-02 17:56:58 +0600

    Add help tooltip to Double Tap binding section in button editor

--- app/src/main/java/com/winlator/cmod/ControlsEditorActivity.java
+++ app/src/main/java/com/winlator/cmod/ControlsEditorActivity.java
@@ -391,7 +391,7 @@ public class ControlsEditorActivity extends AppCompatActivity implements View.On
 
     private void loadDoubleTapBindingSection(final ControlElement element, LinearLayout container) {
         List<Binding> seq = new ArrayList<>(element.getDoubleTapBindings());
-        View section = com.winlator.cmod.widget.BindingSequenceEditor.createView(this, "Double Tap", 0, seq, () -> {
+        View section = com.winlator.cmod.widget.BindingSequenceEditor.createView(this, "Double Tap", R.string.button_double_tap_help, seq, () -> {
             element.setDoubleTapBindings(seq);
             profile.save();
             inputControlsView.invalidate();
